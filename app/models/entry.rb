ed6b8134e86369c2040945ee1b7eefced1a6ffc2
TrầnTrungHiếu_20121699_ASK57 <not_my_name@ymail.com>
2016-04-12 09:11:27 +0000

    Add user following

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,7 +1,7 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  validates :title, presence: true, length: { maximum: 100 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true, length: { maximum: 1000 }
   validates :user_id, presence: true
 end
